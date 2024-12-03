0737f973453f9c536c51d139ec3f80f6af9c52bb
Muhammad Bilal <iMuhammadBilalDev@gmail.com>
2024-12-03 20:14:47 +0500

    changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -39,7 +39,7 @@ const LoginPage = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8100/auth/login", {
+      const response = await fetch("https://mern-auth-one-rho.vercel.app//auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
